0a924189e5135c021a5e4f1028bd1ae53ed4478d
Ruben Nine <ruben@9labs.io>
2020-07-28 15:56:20 +0200

    Adding support for tapping on canvas items from modules that implement `ContainsCanvasItems`.

--- Sources/TransformationsUI/Internal/Modules/Overview/View Controllers/OverviewViewController.swift
+++ Sources/TransformationsUI/Internal/Modules/Overview/View Controllers/OverviewViewController.swift
@@ -14,26 +14,39 @@ protocol OverviewViewControllerDelegate: class {
 }
 
 class OverviewViewController: ModuleViewController {
+    // MARK: - Internal Properties
+
     weak var delegate: OverviewViewControllerDelegate?
 
-    let modules: [EditorModule]
+    // MARK: - Private Properties
 
-    lazy var renderNode = OverviewRenderNode()
-    lazy var modulesToolbar = StandardToolbar(items: modules, style: .modules)
+    private let modules: [EditorModule]
+    private lazy var renderNode = OverviewRenderNode()
 
-    // MARK: - Internal Functions
+    private lazy var toolbar: StandardToolbar = {
+        let toolbar = StandardToolbar(items: modules, style: .modules)
 
-    func getRenderNode() -> RenderNode {
-        return renderNode
+        toolbar.delegate = self
+
+        return toolbar
+    }()
+
+    private lazy var nodesAndModules: [(EditorModule, ContainsCanvasItems)] = modules.compactMap {
+        if let node = $0.viewController.getRenderNode() as? ContainsCanvasItems {
+            return ($0, node)
+        } else {
+            return nil
+        }
     }
 
-    // MARK: - View overrides
+    private lazy var tapGestureRecognizer: UITapGestureRecognizer = {
+        let recognizer = UITapGestureRecognizer()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+        recognizer.numberOfTapsRequired = 1
+        recognizer.addTarget(self, action: #selector(handleTapGesture(recognizer:)))
 
-        setupView()
-    }
+        return recognizer
+    }()
 
     // MARK: - Lifecycle
 
@@ -46,6 +59,64 @@ class OverviewViewController: ModuleViewController {
 
     required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - View overrides
+
+extension OverviewViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupView()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        addGestureRecognizers()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        removeGestureRecognizers()
+    }
+}
+
+// MARK: - EditorModuleVC Conformance
+
+extension OverviewViewController {
+    func getRenderNode() -> RenderNode { renderNode }
+}
+
+// MARK: - Gesture Handling
+
+extension OverviewViewController {
+    @objc func handleTapGesture(recognizer: UITapGestureRecognizer) {
+        for (module, node) in nodesAndModules {
+            if let canvasItem = node.canvasItem(at: recognizer.location(in: imageView)) {
+                node.selectedCanvasItem = canvasItem
+                delegate?.moduleSelected(module: module)
+                break
+            }
+        }
+    }
+}
+
+// MARK: - Private Functions
+
+private extension OverviewViewController {
+    func setupView() {
+        stackView.addArrangedSubview(toolbar)
+    }
+
+    func addGestureRecognizers() {
+        scrollView.addGestureRecognizer(tapGestureRecognizer)
+    }
+
+    func removeGestureRecognizers() {
+        scrollView.removeGestureRecognizer(tapGestureRecognizer)
     }
 }
 
