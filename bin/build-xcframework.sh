87e59d308eb65396dfe3bf56707364485c9315be
Ruben Nine <ruben@9labs.io>
2020-12-17 13:53:42 +0100

    Adding bin/fetch-dependencies.sh, removing Carthage files, etc.

--- bin/build-xcframework.sh
+++ bin/build-xcframework.sh
@@ -4,9 +4,9 @@
 set -o errexit
 
 # Archive using iPhone SDK/
-xcodebuild archive -scheme $SCHEME -configuration Release -sdk iphoneos14.0 -destination "generic/platform=iOS" -archivePath $DEST/$FRAMEWORK.iphoneos.xcarchive OBJROOT=build/iOS | xcpretty
+xcodebuild archive -scheme $SCHEME -configuration Release -sdk iphoneos14.3 -destination "generic/platform=iOS" -archivePath $DEST/$FRAMEWORK.iphoneos.xcarchive OBJROOT=build/iOS | xcpretty
 # Archive using iPhoneSimulator SDK.
-xcodebuild archive -scheme $SCHEME -configuration Release -sdk iphonesimulator14.0 -destination "generic/platform=iOS Simulator" -archivePath $DEST/$FRAMEWORK.iphonesimulator.xcarchive OBJROOT=build/simulator | xcpretty
+xcodebuild archive -scheme $SCHEME -configuration Release -sdk iphonesimulator14.3 -destination "generic/platform=iOS Simulator" -archivePath $DEST/$FRAMEWORK.iphonesimulator.xcarchive OBJROOT=build/simulator | xcpretty
 # Create XCFramework.
 xcodebuild -create-xcframework -framework $DEST/$FRAMEWORK.iphoneos.xcarchive/Products/Library/Frameworks/$FRAMEWORK.framework -framework $DEST/$FRAMEWORK.iphonesimulator.xcarchive/Products/Library/Frameworks/$FRAMEWORK.framework -output $DEST/$FRAMEWORK.xcframework | xcpretty
 # Remove $SCHEME prefix on symbols names (per https://developer.apple.com/forums/thread/123253)
