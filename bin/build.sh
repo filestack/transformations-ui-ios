e30759db8d8f66310c5da0642cc1d5aaabdb84cb
Ruben Nine <ruben@9labs.io>
2021-02-03 11:37:33 +0100

    Updating artifacts.

--- bin/build.sh
+++ bin/build.sh
@@ -9,7 +9,7 @@ DEST=$(dirname "$DIR")/artifacts
 rm -Rf $DEST/*.xcarchive $DEST/*.xcframework
 
 # Build all the required binary dependencies.
-for PACKAGE in "TransformationsUIShared" "TransformationsUIPremiumAddOns" "FilestackSDK" "Filestack" "UberSegmentedControl" "Pikko"; do
+for PACKAGE in "TransformationsUIShared" "TransformationsUIPremiumAddOns" "FilestackSDK" "ObjcDefs" "Filestack" "UberSegmentedControl" "Pikko"; do
 
     case $PACKAGE in
     "TransformationsUIShared")
@@ -21,6 +21,10 @@ for PACKAGE in "TransformationsUIShared" "TransformationsUIPremiumAddOns" "Files
     OWNER="filestack"
     ;;
     "FilestackSDK")
+    REPO="filestack-swift"
+    OWNER="filestack"
+    ;;
+    "ObjcDefs")
     REPO="filestack-swift"
     OWNER="filestack"
     ;;
