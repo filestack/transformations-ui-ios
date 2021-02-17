c06fe75b402069c507c748bf0c1172dc4db2d6d6
Ruben Nine <ruben@9labs.io>
2021-02-17 14:00:53 +0100

    Updating dependency on Filestack to 2.7.1.

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(name: "Filestack", url: "https://github.com/filestack/filestack-ios", .upToNextMajor(from: Version(2, 7, 0))),
+        .package(name: "Filestack", url: "https://github.com/filestack/filestack-ios", .upToNextMajor(from: Version(2, 7, 1))),
         .package(name: "FilestackSDK", url: "https://github.com/filestack/filestack-swift", .upToNextMajor(from: Version(2, 7, 0))),
         .package(name: "Pikko", url: "https://github.com/rnine/Pikko.git", .upToNextMajor(from: Version(1, 0, 9))),
         .package(name: "UberSegmentedControl", url: "https://github.com/rnine/UberSegmentedControl.git", .upToNextMajor(from: Version(1, 3, 4))),
