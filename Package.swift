bea42226d29223485ffd99051e5e85e1c137d83f
Ruben Nine <ruben@9labs.io>
2021-08-16 12:47:02 +0200

    Removing type: .dynamic from library. Updating dependencies.

--- Package.swift
+++ Package.swift
@@ -9,15 +9,14 @@ let package = Package(
     products: [
         .library(
             name: "TransformationsUI",
-            type: .dynamic,
             targets: ["TransformationsUI"]
         )
     ],
     dependencies: [
-        .package(name: "Filestack", url: "https://github.com/filestack/filestack-ios", .upToNextMajor(from: Version(2, 7, 1))),
-        .package(name: "FilestackSDK", url: "https://github.com/filestack/filestack-swift", .upToNextMajor(from: Version(2, 7, 0))),
-        .package(name: "Pikko", url: "https://github.com/rnine/Pikko.git", .upToNextMajor(from: Version(1, 0, 9))),
-        .package(name: "UberSegmentedControl", url: "https://github.com/rnine/UberSegmentedControl.git", .upToNextMajor(from: Version(1, 3, 4))),
+        .package(name: "Filestack", url: "https://github.com/filestack/filestack-ios", .upToNextMajor(from: Version(2, 8, 0))),
+        .package(name: "FilestackSDK", url: "https://github.com/filestack/filestack-swift", .upToNextMajor(from: Version(2, 8, 0))),
+        .package(name: "Pikko", url: "https://github.com/rnine/Pikko.git", .upToNextMajor(from: Version(1, 1, 0))),
+        .package(name: "UberSegmentedControl", url: "https://github.com/rnine/UberSegmentedControl.git", .upToNextMajor(from: Version(1, 3, 5))),
     ],
     targets: [
         .target(
